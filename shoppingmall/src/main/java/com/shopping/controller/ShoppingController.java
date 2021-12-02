7e9e8e8a1c07a425b59ebcac765158adf54fcf29
tj <tj@DESKTOP-G9L5RR7>
2021-12-02 16:34:18 +0900

    컨트롤러 테스트

--- shoppingmall/src/main/java/com/shopping/controller/ShoppingController.java
+++ shoppingmall/src/main/java/com/shopping/controller/ShoppingController.java
@@ -1,8 +1,19 @@
 package com.shopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopping.service.ShoppingService;
 
 @Controller
 public class ShoppingController {
 	
+	@Autowired
+	ShoppingService shoppingService;
+	
+	@GetMapping("shopping/list")
+	public String list() {
+		return "list";
+	}
 }
