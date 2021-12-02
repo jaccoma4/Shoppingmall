bc564bc1dbd2c226e8bff1f8ca3ccf20ab7cf354
tj <tj@DESKTOP-G9L5RR7>
2021-12-02 15:01:15 +0900

    test

---
+++ shoppingmall/src/main/java/com/shopping/entity/User.java
@@ -0,0 +1,15 @@
+package com.shopping.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	private int id;
+	private String name;
+	private String address;
+	private LocalDate birthDate;
+	
+	
+}
---
+++ shoppingmall/src/main/java/com/shopping/mapper/ShoppingMapper.java
@@ -0,0 +1,8 @@
+package com.shopping.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ShoppingMapper {
+
+}
