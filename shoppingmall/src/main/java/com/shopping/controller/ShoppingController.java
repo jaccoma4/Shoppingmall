7d3c92fea2ea5cead32b1ba8ad07d8e594f0349c
tj <tj@DESKTOP-G9L5RR7>
2021-12-02 17:51:23 +0900

    서버 실행 완료

--- shoppingmall/src/main/java/com/shopping/controller/ShoppingController.java
+++ shoppingmall/src/main/java/com/shopping/controller/ShoppingController.java
@@ -3,17 +3,22 @@ package com.shopping.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopping.service.ShoppingService;
 
 @Controller
 public class ShoppingController {
 	
-	@Autowired
-	ShoppingService shoppingService;
 	
 	@GetMapping("shopping/list")
 	public String list() {
 		return "list";
+	}
+	
+	
+	@PostMapping("test")
+	public String test() {
+		return "test";
 	}
 }
---
+++ shoppingmall/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>test</title>
+</head>
+<body>
+	<h1>test</h1>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	<div>test</div>
+	
+</body>
+</html>
